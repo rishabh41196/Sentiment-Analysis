ec155f6a61c7a5ab60d53a569a16f2c9b696d739
yogitamukhija7 <yogita.mukhija7@gmail.com>
2017-05-02 04:28:07 +0530

    Added Twitter Model

--- Minor/Project/minor/sentiment/admin.py
+++ Minor/Project/minor/sentiment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import TweetModel
 # Register your models here.
+
+class TweetModelAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(TweetModel, TweetModelAdmin)
--- Minor/Project/minor/sentiment/models.py
+++ Minor/Project/minor/sentiment/models.py
@@ -2,4 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class TweetModel(models.Model):
+	date = models.DateTimeField()
+	location = models.CharField(max_length = 100)
+	positive = models.IntegerField()
+	negative = models.IntegerField()
+	neutral = models.IntegerField()
+	timestamp=models.DateTimeField(auto_now_add=True,auto_now=False)
+	updated=models.DateTimeField(auto_now_add=False,auto_now=True)
+	class Meta:
+		unique_together = (('date', 'location'),)
+	
+	def __unicode__(self):
+		return self.location
