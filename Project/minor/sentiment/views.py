cb92d2b180340a917f2aae96b87d81ab37f99d0c
shubhigupta96 <96hshubhigupta@gmail.com>
2017-05-04 12:54:29 +0530

    Final Changes

--- Project/minor/sentiment/views.py
+++ Project/minor/sentiment/views.py
@@ -27,7 +27,12 @@ def tweetView(request):
 	final=[]
 	sentiment = []
 	subj=request.session.get('form')['searchBox']		
-		
+
+ 	if request.POST.get('timestamp') is not None:
+ 		received_datetime = datetime.strptime(request.POST.get('timestamp'),  '%d-%m-%Y %H:%M:%S')
+ 		searched_date = received_datetime.date()
+
+
 	context = {
 		'subj' : subj,
 		'pos': 0,
@@ -41,25 +46,29 @@ def tweetView(request):
 		'lis' : lis
 	}
 	
+
+
 	if form.is_valid():
 		if not subj:
 			return render(request,'tweetView.html',context)
-
-		obj=TwitterObject(subj)
-		tweets = obj.fetchTweets()
-		
+		tweets=[]
+		if str(searched_date) == str(datetime.now().date()):
+			obj=TwitterObject(subj)
+			tweets = obj.fetchTweets()
+			
 		dbTweets = TweetModel.objects.all().filter(topic = subj, date=str(searched_date))
 		tweetId = []
 		for tweet in dbTweets :
-			sentData = {}
-			lat.append(float(tweet.lat))
-			lon.append(float(tweet.lon))
-			sentiment.append(tweet.sentiment)
-			sentData['latitude'] = tweet.lat
-			sentData['longitude'] = tweet.lon
-			sentData['sentiment'] = tweet.sentiment
-			final.append(sentData)
-	
+			if str(tweet.date) == str(searched_date):
+				sentData = {}
+				lat.append(float(tweet.lat))
+				lon.append(float(tweet.lon))
+				sentiment.append(tweet.sentiment)
+				sentData['latitude'] = tweet.lat
+				sentData['longitude'] = tweet.lon
+				sentData['sentiment'] = tweet.sentiment
+				final.append(sentData)
+		
 		for tweet in tweets:
 			# print tweet.get('location')
 			entity = TweetModel(tweetId = tweet.get('id'), 
@@ -70,18 +79,17 @@ def tweetView(request):
 				lon = tweet.get('long'),
 				sentiment = tweet.get('sentiment'))
 			if entity.tweetId not in tweetId:
-				sentData={}
-				sentData['latitude'] = entity.lat
-				sentData['longitude'] = entity.lon
-				sentData['sentiment'] = entity.sentiment
-				lat.append(float(entity.lat))
-				lon.append(float(entity.lon))
-				sentiment.append(entity.sentiment)			
-				final.append(sentData)
+				if str(entity.date) == str(searched_date):
+					sentData={}
+					sentData['latitude'] = entity.lat
+					sentData['longitude'] = entity.lon
+					sentData['sentiment'] = entity.sentiment
+					lat.append(float(entity.lat))
+					lon.append(float(entity.lon))
+					sentiment.append(entity.sentiment)			
+					final.append(sentData)
 				tweetId.append(entity.tweetId)
 				entity.save()	
-		f = open(os.path.join(settings.STATIC_ROOT,"json","data.json"),"w")
-		f.write('{"locations" : [')
 		ct=0
 		l= len(final)
 
@@ -92,6 +100,7 @@ def tweetView(request):
 		sentiment = json.dumps(sentiment)
 		lon=json.dumps(lon)
 		lat=json.dumps(lat)
+		
 		for tweet in final:
 			if(tweet['sentiment']=='positive'):
 				pos+=1;
